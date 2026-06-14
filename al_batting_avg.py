904b92d1a34802f08cd3365ac4c6c51a173774e6
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-06-14 12:32:44 -0400

    Update al_batting_avg.py

--- al_batting_avg.py
+++ al_batting_avg.py
@@ -131,7 +131,9 @@ leaderboards = {
     "strikeouts": get_pitching_leaders("strikeOuts"),
     "whip": get_pitching_leaders("whip"),
     "innings": get_pitching_leaders("inningsPitched"),
-    "saves": get_pitching_leaders("saves")
+    "quality_starts": get_pitching_leaders("qualityStarts"),
+    "saves": get_pitching_leaders("saves"),
+    "holds": get_pitching_leaders("holds")
 }}
 
 with open("leaderboards.json", "w") as f:
