53b05630c668bb0a2ca1d773e91899ad417e5ad5
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-08 19:59:44 -0400

    Create leaders.py

---
+++ leaders.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime
+
+import statsapi
+
+SEASON = datetime.now().year
+
+TEAM_ABBR = {
+    108: "LAA",
+    109: "ARI",
+    110: "BAL",
+    111: "BOS",
+    112: "CHC",
+    113: "CIN",
+    114: "CLE",
+    115: "COL",
+    116: "DET",
+    117: "HOU",
+    118: "KCR",
+    119: "LAD",
+    120: "WSH",
+    121: "NYM",
+    133: "ATH",
+    134: "PIT",
+    135: "SDP",
+    136: "SEA",
+    137: "SFG",
+    138: "STL",
+    139: "TBR",
+    140: "TEX",
+    141: "TOR",
+    142: "MIN",
+    143: "PHI",
+    144: "ATL",
+    145: "CHW",
+    146: "MIA",
+    147: "NYY",
+    158: "MIL"
+}
