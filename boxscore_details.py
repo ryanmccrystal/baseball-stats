23506cc7acbddaaa2168602be5611d1f8a8a8b36
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-17 10:42:57 -0400

    Update boxscore_details.py

--- boxscore_details.py
+++ boxscore_details.py
@@ -31,9 +31,39 @@ for game in schedule:
         }
     )
 
+    away = linescore["teams"]["away"]
+    home = linescore["teams"]["home"]
+
+    away_innings = []
+    home_innings = []
+
+    for inning in linescore["innings"]:
+        away_innings.append(inning["away"]["runs"])
+        home_innings.append(inning["home"]["runs"])
+
     output["games"].append({
-        "gamePk": gamePk,
-        "linescore": linescore
+
+        "headline": f'{game["away_name"].upper()} {away["runs"]}, '
+                f'{game["home_name"].upper()} {home["runs"]}',
+
+        "away": {
+            "city": game["away_name"],
+            "nickname": game["away_team"],
+            "innings": away_innings,
+            "runs": away["runs"],
+            "hits": away["hits"],
+            "errors": away["errors"]
+        },
+
+        "home": {
+            "city": game["home_name"],
+            "nickname": game["home_team"],
+            "innings": home_innings,
+            "runs": home["runs"],
+            "hits": home["hits"],
+            "errors": home["errors"]
+        }
+
     })
 
 print("Writing JSON...")
