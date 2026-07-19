f90ee3dbc73aa70bc78bb4c1e120a987a7418097
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-07-18 22:03:35 -0400

    Update boxscore_details.py

--- boxscore_details.py
+++ boxscore_details.py
@@ -18,6 +18,43 @@ output = {
     "games": []
 }
 
+def nickname(team_name):
+
+    names = {
+        "Arizona Diamondbacks": "DBacks",
+        "Atlanta Braves": "Braves",
+        "Baltimore Orioles": "Orioles",
+        "Boston Red Sox": "Red Sox",
+        "Chicago Cubs": "Cubs",
+        "Chicago White Sox": "White Sox",
+        "Cincinnati Reds": "Reds",
+        "Cleveland Guardians": "Guardians",
+        "Colorado Rockies": "Rockies",
+        "Detroit Tigers": "Tigers",
+        "Houston Astros": "Astros",
+        "Kansas City Royals": "Royals",
+        "Los Angeles Angels": "Angels",
+        "Los Angeles Dodgers": "Dodgers",
+        "Miami Marlins": "Marlins",
+        "Milwaukee Brewers": "Brewers",
+        "Minnesota Twins": "Twins",
+        "New York Mets": "Mets",
+        "New York Yankees": "Yankees",
+        "Athletics": "Athletics",
+        "Philadelphia Phillies": "Phillies",
+        "Pittsburgh Pirates": "Pirates",
+        "San Diego Padres": "Padres",
+        "San Francisco Giants": "Giants",
+        "Seattle Mariners": "Mariners",
+        "St. Louis Cardinals": "Cardinals",
+        "Tampa Bay Rays": "Rays",
+        "Texas Rangers": "Rangers",
+        "Toronto Blue Jays": "Blue Jays",
+        "Washington Nationals": "Nationals"
+    }
+
+    return names.get(team_name, team_name)
+
 def split_team_name(full_name):
     parts = full_name.split()
     city = " ".join(parts[:-1])
